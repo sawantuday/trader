51aadf49435a69232033709d6927a64e9b03f6c7
uday sawant <uday@sweetcouch.com>
2014-11-20 18:13:14 +0530

    small updates

--- src/trader/AbstractTrader.java
+++ src/trader/AbstractTrader.java
@@ -1,5 +1,23 @@
 package trader;
 
 public abstract class AbstractTrader {
+	private double cash;
+	private double btc;
+	private int sellPercent;
+	private int buyPercent;
+	private int cashReserve;
+	private int btcReserve;
+	private int commition;
+	
+	/**
+	 * this method will be override by child classes 
+	 * it will give context to trade against
+	 */
+	public abstract void handleData();
+	
+	protected void storeResults(){}
+	
+	protected void order(){}
+	
 	
 }
